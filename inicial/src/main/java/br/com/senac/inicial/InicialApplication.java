840a6cf5945ec1e28dfd1a5ec2ab150fae4b4a30
Igor Eugenio <igoreugenio634@gmail.com>
2026-07-29 21:21:11 -0300

    primeira aula

---
+++ inicial/src/main/java/br/com/senac/inicial/InicialApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicialApplication.class, args);
+	}
+
+}
