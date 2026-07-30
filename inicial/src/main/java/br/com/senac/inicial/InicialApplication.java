6a6703ead3b40e269d7cf6e0e67d5572b723c065
Igor Eugenio <igoreugenio634@gmail.com>
2026-07-29 21:36:35 -0300

    Segunda Alteração

--- inicial/src/main/java/br/com/senac/inicial/InicialApplication.java
+++ inicial/src/main/java/br/com/senac/inicial/InicialApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InicialApplication {
 
 	// primeira alteração
+
 	public static void main(String[] args) {
 		SpringApplication.run(InicialApplication.class, args);
 	}
